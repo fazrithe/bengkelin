6ccc19f5f864d2b5bdd92b3879e9d6a2bf659beb
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-02-15 11:35:32 +0700

    update package

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return(
     <Grid item xs={12} sm={6} className="mt-4 mb-2">
             <CardContent sx={{ justifyContent: 'flex-end', textAlign: 'center' }}>
-                E-Commerce
+                bengkelin
             </CardContent>
         </Grid>
     )
